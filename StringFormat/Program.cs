406f274965936f51baefb76f26b6e96478a3c823
Martin <strimpfl@hotmail.com>
2019-05-02 09:37:59 +0200

    Add project files.

---
+++ StringFormat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var meetingTime =
+                new MeetingTime
+                {
+                    Day = DayOfWeek.Monday,
+                    StartTime = new TimeSpan(10, 0, 0),
+                    EndTime = new TimeSpan(12, 0, 0)
+                };
+
+            var compositeFormat = "The meeting {0}: {1} - {2} is in a conflict.";
+
+            var compositeMessage = string.Format(compositeFormat, meetingTime.Day, meetingTime.StartTime, meetingTime.EndTime);
+
+            var interpolatedMessage = $"The meeting {meetingTime.Day}: {meetingTime.StartTime} - {meetingTime.EndTime} is in a conflict.";
+
+            Console.WriteLine(compositeMessage);
+            Console.WriteLine(interpolatedMessage);
+        }
+    }
+
+    public class MeetingTime
+    {
+        public DayOfWeek Day { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+    }
+}
